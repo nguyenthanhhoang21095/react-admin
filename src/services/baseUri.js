0dce8fd546ed22a231259d29548f93d14cfed8e8
nguyenthanhhoang21095 <thanhhoang021095.work@gmail.com>
2021-06-02 17:22:23 +0700

    fix error build in production

--- src/services/baseUri.js
+++ src/services/baseUri.js
@@ -1,5 +1,5 @@
 // const devUrl = 'http://localhost:2021/api/';
 const prodUrl = 'https://server.hoang-shop-web.xyz/api/';
-const baseUri =  'https://server.hoang-shop-web.xyz/api/';
+const baseUri = prodUrl;
 
 export default baseUri;
